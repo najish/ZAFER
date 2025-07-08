6f26e8126e3bbd7b32a5df96c2ad48f52c4e72af
anonymous <anonymos@gmail.com>
2025-07-08 17:32:36 +0530

    testing false case

--- unittests/test_multi.py
+++ unittests/test_multi.py
@@ -7,9 +7,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from multi import multi
 
 def test_multi_positive_numbers():
-    assert multi(5, 18) == 90
+    assert multi(5, 18) == -90
 
 def test_multi_mixed_numbers():
     assert multi(-5,2) == -10
 
-    
